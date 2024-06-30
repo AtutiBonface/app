8644c8033da628d33eaacecb82dcc0bfc69a1e5d
AtutiBonface <xengine.atutibonface.developer@gmail.com>
2024-06-30 01:45:17 -0700

    commit

--- home_page.py
+++ home_page.py
@@ -1,5 +1,9 @@
 import customtkinter as ctk
 from customtkinter import CTkFont
+from customtkinter import filedialog
+from pathlib import Path
+import os 
+from urllib.parse import urlparse
 import app_utils
 class Home():
     def __init__(self, parent):
@@ -11,7 +15,7 @@ class Home():
         self.address_label = ctk.CTkLabel(self.enter_link_box,text_color='white',  font=self.font,text='Address').place(y=55, x=20)
         self.filename_label = ctk.CTkLabel(self.enter_link_box,text_color='white', font=self.font,text='Filename').place(y=90, x=20)
 
-        self.status_label = ctk.CTkLabel(self.enter_link_box, text='xxx')
+        self.status_label = ctk.CTkLabel(self.enter_link_box, text='')
         self.status_label.pack(pady=10)
 
         self.link_text = ctk.StringVar()
@@ -24,10 +28,10 @@ class Home():
         self.directory_box = ctk.CTkFrame(self.enter_link_box, fg_color='transparent')
         self.filename_entry = ctk.CTkEntry(self.directory_box, border_width=0, width=200, textvariable=self.filename_text,font=CTkFont(weight='bold', family='Helvetica', size=10), placeholder_text='file name', height=25)
         self.filename_entry.pack(side='left', fill='x')
-        self.change_folder_btn = ctk.CTkButton(self.directory_box, text='', cursor='hand2', image=self.xe_images.folderImg, fg_color='#3d539f',width=20, height=25, hover=False)
+        self.change_folder_btn = ctk.CTkButton(self.directory_box, text='',command=self.openDownloadToFolder, cursor='hand2', image=self.xe_images.folderImg, fg_color='#3d539f',width=20, height=25, hover=False)
         self.change_folder_btn.pack(side='left')
         self.directory_box.pack()
-        self.start_download = ctk.CTkButton(self.enter_link_box,hover=False, cursor='hand2', text='Download',font=CTkFont(weight='bold', family='Helvetica', size=11), width=120, height=40, corner_radius=15, fg_color='#5b74d8')
+        self.start_download = ctk.CTkButton(self.enter_link_box,hover=False,command=self.startDownload, cursor='hand2', text='Download',font=CTkFont(weight='bold', family='Helvetica', size=11), width=120, height=40, corner_radius=15, fg_color='#5b74d8')
         self.start_download.pack(pady=15)
 
         
@@ -35,8 +39,64 @@ class Home():
         self.link_entry.bind('<KeyRelease>', self.getInputValue)
 
         app_utils.DownloadingIndicatorBox(parent)
-    def getInputValue(self, event):
-        print(self.link_text.get())
+    def getInputValue(self, event):        
+        
+
+        link = self.link_text.get()
+        filename = self.filename_text.get() 
+
+       
+        if not urlparse(link).scheme:
+            link = f'http://{link}'
+
+        url_parsed = urlparse(link)
+
+        if os.path.basename(url_parsed.path):
+            filename = os.path.basename(url_parsed.path)
+
+            self.filename_text.set(filename)
+        else:
+            self.filename_text.set('FILE')
+
+
+    def returnFilename(self, path):
+        pass
+
+    def openDownloadToFolder(self):
+        home = Path.home()
+        file_location = filedialog.askdirectory(mustexist=True,initialdir=home, title='Select Folder')
+
+    def startDownload(self):
+        link = self.link_text.get()
+        filename = self.filename_text.get() 
+
+       
+        if not urlparse(link).scheme:
+            link = f'http://{link}'
+
+        if not urlparse(link).netloc:
+            self.status_label.configure(text='Insert correct address!', text_color='orange')
 
 
         
+        else:
+            url_parsed = urlparse(link)
+
+            if '.' in link:
+                
+                initial_filename = self.filename_text.get()
+                name, extension = os.path.splitext(initial_filename)
+
+                if not extension:
+                    extension = '.html'
+                if not name:
+                    self.status_label.configure(text=f'No file name!', text_color='orange')
+                 
+                else:
+                    filename_and_path = name + extension
+                    
+                    filename = os.path.basename(filename_and_path)
+
+
+
+
